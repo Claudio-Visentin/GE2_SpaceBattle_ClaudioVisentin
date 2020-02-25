99186490c6de7b865ed244a7e229f02f6dc4a4e9
Claudio-Visentin <D16126698@mydit.ie>
2020-02-25 12:05:01 +0000

    Improved demo scene

---
+++ Project/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    Rigidbody rigid;
+    public float speedx;
+    public float speedy;
+    public float speedz;
+    void Start()
+    {
+       rigid = GetComponent <Rigidbody> (); 
+       rigid.AddForce (speedx, speedy, speedz);
+    }
+}

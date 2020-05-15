d539a5fb4d4728d54b5a79648c5b209827143565
Claudio-Visentin <D16126698@mydit.ie>
2020-05-15 16:23:04 +0100

    Created scripts

---
+++ Project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public SceneDirector scene;
+    public Transform spaceship;
+    public float speed;
+    void Update()
+    {
+        if (scene.Scene == "Scene1")
+        {
+            transform.LookAt(spaceship.position);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3 (0f, 0f, 0f), speed);
+        }
+        if (scene.Scene == "Scene2")
+        {
+            //Clip plays
+        }
+        if (scene.Scene == "Scene3")
+        {
+
+        }
+        if (scene.Scene == "Scene4")
+        {
+
+        }
+        if (scene.Scene == "Scene5")
+        {
+            //Clip plays
+        }
+        if (scene.Scene == "Scene6")
+        {
+
+        }
+    }
+}
---
+++ Project/Assets/Scripts/SceneDirector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine.Video;
+using UnityEngine;
+
+public class SceneDirector : MonoBehaviour
+{
+    public VideoPlayer video;
+    public VideoClip scene2;
+    public VideoClip scene5;
+    public string Scene;
+
+    void Start()
+    {
+        video.renderMode = VideoRenderMode.CameraNearPlane;
+        StartCoroutine("SceneTimer"); 
+    }
+
+   IEnumerator SceneTimer ()
+    {
+        Scene = "Scene1";
+        yield return new WaitForSeconds(8f);
+        video.clip = scene2;
+        video.Play();
+        Scene = "Scene2";
+        yield return new WaitForSeconds(20f);
+        video.Stop();
+        Scene = "Scene3";
+        yield return new WaitForSeconds(5f);
+        Scene = "Scene4";
+        yield return new WaitForSeconds(9f);
+        Scene = "Scene5";
+        video.clip = scene5;
+        video.Play();
+        yield return new WaitForSeconds(8f);
+        Scene = "Scene6";
+        video.Stop();
+    }
+}

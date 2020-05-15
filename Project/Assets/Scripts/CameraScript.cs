c7be219b6ee7904fd89cdf400d5af61620a5a64d
Claudio-Visentin <D16126698@mydit.ie>
2020-05-15 17:10:23 +0100

    Work on scene 1

--- Project/Assets/Scripts/CameraScript.cs
+++ Project/Assets/Scripts/CameraScript.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class CameraScript : MonoBehaviour
 {
     public SceneDirector scene;
     public Transform spaceship;
     public float speed;
+
+    bool scene1part1check = false;
+    bool scene1part1 = false;
     void Update()
     {
         if (scene.Scene == "Scene1")
         {
-            transform.LookAt(spaceship.position);
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3 (0f, 0f, 0f), speed);
+            if (scene1part1check == false)
+            {
+                StartCoroutine("Scene1");
+                scene1part1check = true;
+            }
+            if (scene1part1 == false)
+            {
+                transform.LookAt(spaceship.position);
+                transform.parent = spaceship.transform;
+            }
+            else
+            {
+                transform.parent = null;
+                transform.LookAt(spaceship.position);
+                transform.position = Vector3.MoveTowards(transform.position, spaceship.position, speed);
+            }
+            
         }
         if (scene.Scene == "Scene2")
         {
@@ -32,5 +51,10 @@ public class CameraScript : MonoBehaviour
         {
 
         }
+    }
+    public IEnumerator Scene1()
+    {
+        yield return new WaitForSeconds(2f);
+        scene1part1 = true;
     }
 }

a35b7d4b028cf84bdeb388b9f4fd22c5bcb19419
Claudio-Visentin <D16126698@mydit.ie>
2020-05-16 11:38:03 +0100

    Added friendly AI

---
+++ Project/Assets/Scripts/FriendAI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FriendAI : MonoBehaviour
+{
+    public Transform mainship;
+    public Vector3 offset;
+    public float speed;
+    void Update()
+    {
+        Vector3 randomfactor = new Vector3 (Random.Range(0, 2), 0f, Random.Range(0, 2));
+        Vector3 nextmove = mainship.position + offset;
+        transform.position = Vector3.MoveTowards(transform.position, nextmove, speed);
+        transform.rotation = mainship.rotation;
+    }
+}

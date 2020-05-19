6309fcdd33b1690d39950c1186d4688f9155bfb0
Claudio-Visentin <D16126698@mydit.ie>
2020-05-20 00:11:52 +0100

    Almost finished

---
+++ Project/Assets/Scripts/UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject ui;
+
+    void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    // Update is called once per frame
+    public void OnStart ()
+    {
+        Time.timeScale = 1;
+        ui.SetActive(false);
+    }
+}

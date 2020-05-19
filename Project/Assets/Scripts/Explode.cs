cb244610db0b2ba97bd4108e19b0f0fc8d46e439
Claudio-Visentin <D16126698@mydit.ie>
2020-05-19 20:34:19 +0100

    Adding lasers and explosions

---
+++ Project/Assets/Scripts/Explode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public float timer;
+    public GameObject explosion;
+    public GameObject spaceship;
+    void Awake()
+    {
+        StartCoroutine("Exploder");
+    }
+
+    // Update is called once per frame
+    IEnumerator Exploder ()
+    {
+        yield return new WaitForSeconds(timer);
+        GameObject explosioninst = Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(spaceship);
+    }
+}
---
+++ Project/Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public float wait;
+    public GameObject laser;
+    public Vector3 direction;
+    public Transform laserstarter;
+    public bool repeat;
+
+    void Awake()
+    {
+        StartCoroutine("Shoot");
+    }
+
+    IEnumerator Shoot ()
+    {
+        yield return new WaitForSeconds(wait);
+        GameObject instlaser = Instantiate(laser, laserstarter.transform);
+        instlaser.transform.parent = laserstarter;
+        instlaser.GetComponent<Rigidbody>().AddRelativeForce(direction);
+        yield return new WaitForSeconds(0.8f);
+        Destroy(instlaser);
+        if (repeat == true)
+        {
+            StartCoroutine("Shoot");
+        }
+    }
+    
+}
